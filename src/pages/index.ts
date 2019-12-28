437694e84bb02cd319db05a9978d608d04f8c079
everbrez <everbrez@gmail.com>
2019-12-28 23:16:08 +0800

    :dizzy: feature (register) add register

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
 import Login from './login/login'
+import Register from './register/register'
 
 export {
   Login,
+  Register,
 }
---
+++ src/pages/register/register.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register: React.FC = props => {
+  return (
+    <div></div>
+  )
+}
+
+export default Register
