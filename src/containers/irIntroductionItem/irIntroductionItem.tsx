13e9a010be97832cba77045bfa15eb27baa74897
everbrez <everbrez@gmail.com>
2020-01-10 19:17:32 +0800

    :dizzy: feature (page) add product

---
+++ src/containers/irIntroductionItem/irIntroductionItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Classes from './irIntroductionItem.module.scss'
+
+const IrIntroductionItem: React.FC = props => {
+
+  return (
+    <div></div>
+  )
+}
+
+export default IrIntroductionItem
--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Container } from '../../containers'
+import { Button } from '../../components'
+import Classes from './product.module.scss'
+import triangleTop from '../../assets/images/triangle-top.svg'
+import irDataImg from '../../assets/images/ir-data.webp'
 
 const Product: React.FC = props => {
   return (
     <div>
-      Product
+      <section className={Classes['main-section']}>
+        <Container>
+          <h1>
+            IR Analyser
+          </h1>
+
+          <p className={Classes.description}>
+          We use deep learning to analyze chemical graphics
+          <br/>
+          helping scientist more easier to find out the important
+          <br/>
+          infomation from chemical graphics.
+        </p>
+
+          <Button type="gradient" size="large">
+            Have a Try
+          </Button>
+          <Button type="normal" size="large">
+            Learn More
+          </Button>
+
+          <img src={irDataImg} alt="irData" className={Classes['ir-data']} />
+          <img src={triangleTop} className={Classes['triangle-top']} />
+        </Container>
+      </section>
+
     </div>
   )
 }
